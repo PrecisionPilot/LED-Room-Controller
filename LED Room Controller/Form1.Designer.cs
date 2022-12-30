3772995620b287690f123d39d893aa9f329f11eb
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-12-29 20:45:26 -0500

    Update Form1.Designer.cs

--- LED Room Controller/Form1.Designer.cs
+++ LED Room Controller/Form1.Designer.cs
@@ -131,7 +131,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "LED Room Controller";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
